f95093e6fac435ae2148165c6f88b8601be51b5c
stevocurtis <Conway23>
2019-11-26 14:08:32 +0000

    Initial version

---
+++ src/main/java/com/stevo/curtis/SpringIntegrationPlaypenApplication.java
@@ -0,0 +1,13 @@
+package com.stevo.curtis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationPlaypenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationPlaypenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stevo/curtis/SpringIntegrationPlaypenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevo.curtis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationPlaypenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
